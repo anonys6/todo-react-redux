4fa9b22949e47001d89027d9bcc8aa7fd4b54e9f
anonys6 <shakibalam998@gmail.com>
2024-10-24 03:37:48 +0530

    basic setup of react, vite, tailwind, shadcn

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "./components/mode-toggle";
+
+function App() {
+  return (
+    <ModeToggle />
+  );
+}
+
+export default App;
